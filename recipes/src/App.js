59c6986353ba2e3cf1fba0f6e8b834e02c55f004
OmarJ <omarjandali93@gmail.com>
2021-10-19 14:28:45 -0700

    added the ingredients and ingredients list that will display the ingredients

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -12,14 +12,48 @@ const sampleRecipes = [
     name: 'Plain Chicken',
     servings: 3,
     cookTime: '1:45',
-    instructions: '1. Put salt on Chicken\n2. Put chicken in oven\n3. Eat Chicken'
+    instructions: '1. Put salt on Chicken\n2. Put chicken in oven\n3. Eat Chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'chicken',
+        amount: '2 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbsp.'
+      },
+    ]
   },
   {
     id: 2,
     name: 'Herbed Chicken',
     servings: 4,
     cookTime: '3:15',
-    instructions: '1. Season the Chicken\n2. Bake the chicken in the oven\n3. Eat Chicken'
+    instructions: '1. Season the Chicken\n2. Bake the chicken in the oven\n3. Eat Chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'chicken',
+        amount: '2 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbsp.'
+      },
+      {
+        id: 3,
+        name: 'Pepper',
+        amount: '1 Tbsp.'
+      },
+      {
+        id: 4,
+        name: 'Oil',
+        amount: '2.5 Tbsp.'
+      },
+    ]
   }
 ]
 
