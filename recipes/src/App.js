072d10385a5ae32500dc73af95c85b2b51c98d4d
OmarJ <omarjandali93@gmail.com>
2021-10-19 10:24:06 -0700

    added the first set of components including the recipe list. displayed information is based on dummy data

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,7 +1,26 @@
+import RecipeList from './components/RecipeList'
+
 function App() {
   return (
-    <>Hello World</>
+    <RecipeList recipes={sampleRecipes}/>
   );
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: '1. Put salt on Chicken\n2. Put chicken in oven\n3. Eat Chicken'
+  },
+  {
+    id: 2,
+    name: 'Herbed Chicken',
+    servings: 4,
+    cookTime: '3:15',
+    instructions: '1. Season the Chicken\n2. Bake the chicken in the oven\n3. Eat Chicken'
+  }
+]
 
 export default App;
