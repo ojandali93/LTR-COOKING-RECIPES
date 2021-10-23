65aa4f9091505247ec2775e9eea3693a0805304f
OmarJ <omarjandali93@gmail.com>
2021-10-23 15:21:58 -0700

    added the ability to dynaically change each of the recipes when clicking on edit. It is all automaticaly generation on the spot

--- recipes/src/components/RecipeEdit.js
+++ recipes/src/components/RecipeEdit.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import RecipeIngredientsEdit from './RecipeIngredientsEdit'
+import { RecipeContext } from '../App'
 
 export default function RecipeEdit({ recipe }) {
+  const { handleRecipeChange } = useContext(RecipeContext)
+
+  function handleChange(changes){
+    handleRecipeChange(recipe.id, { ...recipe, ...changes})
+  }
+
+  function handleIngredientChange(id, ingredient){
+    const newIngredients = [...recipe.ingredients]
+    const index = newIngredients.findIndex(i => i.id === id)
+    newIngredients[index] = ingredient
+    handleChange({ ingredients: newIngredients })
+  }
+   
   return (
     <div className="recipe-edit">
-      <div>
-        <button>&times;</button>
+      <div className="recipe-edit__remove-button-container">
+        <button className="btn recipe-edit__remove-button">&times;</button>
       </div>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" value={recipe.name}/>
+        <input type="text" name="name" id="name" onInput={e => handleChange({name: e.target.value})} value={recipe.name}/>
         <label htmlFor="cookTime">CookTime</label>
-        <input type="text" name="cookTime" id="cookTime" value={recipe.cookTime}/>
+        <input type="text" name="cookTime" id="cookTime" onInput={e => handleChange({cookTime: e.target.value})} value={recipe.cookTime}/>
         <label htmlFor="servings">Servings</label>
-        <input type="text" name="servings" id="servings" value={recipe.servings}/>
+        <input type="text" name="servings" id="servings" onInput={e => handleChange({servings: parseInt(e.target.value) || ''})} value={recipe.servings}/>
         <label htmlFor="instructions">Instructions</label>
-        <textarea type="textarea" name="instructions" id="instructions" value={recipe.instructions}></textarea>
+        <textarea type="textarea" name="instructions" id="instructions" onInput={e => handleChange({cookTime: e.target.value})} value={recipe.instructions}></textarea>
       </div>
       <br/>
       <label>
@@ -26,7 +40,7 @@ export default function RecipeEdit({ recipe }) {
         <div>Amount</div>
         <div></div>
         {recipe.ingredients.map(ingredient => {
-          return <RecipeIngredientsEdit key={ingredient.id} ingredient={ingredient}/>
+          return <RecipeIngredientsEdit key={ingredient.id} handleIngredientChange={handleIngredientChange} ingredient={ingredient}/>
         })}
       </div>
       <div>
